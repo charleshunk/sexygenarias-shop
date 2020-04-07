b822fb9adf887f63b3c0355c26b6b9d78d8eb69e
Carlos Galan <carlos.galan@gmail.com>
2020-04-07 07:03:02 +0100

    multiple items to visual dropdown cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,16 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemToAdd.id);
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
 
     if (existingCartItem) {
+        console.log('Exists!');
         return cartItems.map(cartItem =>
-            cartItems.id === cartItemToAdd.id
+            cartItem.id === cartItemToAdd.id
             ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem
-            )
+            );
     }
-
+    console.log("Doesn't exist!");
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
